8c587db7a026361eaac203a1fa40d4d7264ea2db
Pedro Miranda <mdeapedro@gmail.com>
2023-07-17 09:52:09 -0300

    Update template-2

--- template-2.cpp
+++ template-2.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+template <typename T> using vt = vector<T>;
 #define iab(i, a, b) for (int i = a; i < b; i++)
 #define iba(i, b, a) for (int i = b-1; i >= a; i--)
 #define i0n(i, n) iab(i, 0, n)
@@ -11,8 +12,18 @@ template <typename ...T> void pr(T ...t) { ((cout << t << ' '), ...); }
 template <typename ...T> void prn(T ...t) { pr(t...); cout << '\n'; }
 template <typename ...T> void re(T &...t) { (cin >> ... >> t); }
 #define def(type, Args...) type Args; re(Args)
-#define TESTCASE def(int, ntc); iab(tci, 1, ntc+1)
-void solution();
-int main() { ios_base::sync_with_stdio(false); cin.tie(nullptr); solution(); return 0; }
 
-void solution() {}
+
+// #define USETC
+void solve(int tci) {}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    #ifdef USETC
+        def(int, ntc); iab(tci, 1, ntc+1) solve(tci);
+    #else
+        solve(0);
+    #endif
+}
